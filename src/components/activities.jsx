7aefd31b47b083df5fb330086e01314c0e76d4ef
gruntcole <gruntcole1998@gmail.com>
2023-08-04 16:56:17 -0400

    added in the components, and worked on login and register

---
+++ src/components/activities.jsx
@@ -0,0 +1,13 @@
+//As an unregistered visitor on the Activities tab, I want to:
+// -see a list of all activities which have been created
+
+// As a registered user on the Activities tab, I want to:
+// -be shown a form to create a new activity (by name and description)
+// -be shown an error if the activity already exists
+
+// STRETCH GOALS
+// As any user on the Activities tab, I want to:
+// -be able to click on an activity name and see a list of all public routines which feature it
+
+// As a registered user on the Activities tab, I want to:
+// -be able to edit an existing activity, and update the description, regardless of who owns it
---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+//export all components
+export { default as Login } from "./login.jsx";
+export { default as Register } from "./register.jsx";
+export { default as Navbar } from "./navBar.jsx";
