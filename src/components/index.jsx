182e147396deb004ac5f0c54dbebdc2240f5068b
Dragonsofash <agpaoajonathanb@gmail.com>
2023-08-06 16:47:48 -0400

    Issues with renaming components.

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,7 +2,7 @@
 export { default as Login } from "./Login.jsx";
 export { default as Register } from "./Register.jsx";
 export { default as Navbar } from "./Navbar.jsx";
-export { default as Activities } from "./Activities.jsx"
-export { default as Routines } from "./Routines.jsx"
-export { default as MyRoutines } from "./MyRoutines.jsx"
+export { default as Routines } from "./Routines.jsx";
+export { default as Activities } from "./Activities.jsx";
+export { default as MyRoutines } from "./MyRoutines.jsx";
 
